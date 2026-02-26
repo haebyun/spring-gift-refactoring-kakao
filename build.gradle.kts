66dcb31d7e90fed0833e80240b905f7d16155568
ikjeong <jipark7937@gmail.com>
2026-02-26 16:51:09 +0900

    refactor: Kotlin 관련 빌드 설정 제거
    Co-authored-by: haebyun <haepal330@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("com.diffplug.spotless") version "7.0.2"
     id("org.flywaydb.flyway") version "12.0.1"
 }
@@ -26,7 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.security:spring-security-crypto")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     implementation(platform("io.jsonwebtoken:jjwt-bom:0.13.0"))
@@ -36,31 +34,12 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.rest-assured:rest-assured:5.5.1")
     testImplementation("io.cucumber:cucumber-java:7.21.1")
     testImplementation("io.cucumber:cucumber-spring:7.21.1")
     testImplementation("io.cucumber:cucumber-junit-platform-engine:7.21.1")
     testImplementation("org.junit.platform:junit-platform-suite")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-spotless {
-    java {
-        target("src/*/java/**/*.java")
-        palantirJavaFormat()
-        removeUnusedImports()
-    }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
-
-ktlint {
-    verbose.set(true)
 }
 
 spotless {
